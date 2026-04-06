8a16f33b51462e54a8788b4be1a73a689f3ea8de
Sourabh Tomar <sourabhtomar6709@gmail.com>
2026-04-06 23:04:35 +0530

    feat: implement binary tree traversals and utilities
    added recursive inorder, preorder and postorder traversals along with tree height and size calculation

--- python/L2/binary_search_trees.py
+++ python/L2/binary_search_trees.py
@@ -316,3 +316,212 @@ def parse_tuple(data): # Defining a parameter automatically creates a variable t
         node = TreeNode(data)
     return node
 
+
+
+# I wrote a detailed explanation of what the data flow looks like for the above written function on pen and paper so i wont spend more time writing it here for now ill move forward and write it as docs here when i come back here to revise 
+
+
+tree2 = parse_tuple(tree_tuple)
+print(tree2.key) # main root node (2)
+print(tree2.left.key,tree2.right.key) # next nodes in tree (3,5)
+print(tree2.left.left.key, tree2.left.right, tree2.right.left.key, tree2.right.right.key) # next nodes (1,None,3,7)
+print(tree2.right.left.right.key, tree2.right.right.left.key, tree2.right.right.right.key) # nodes of nodes (3,7) :- (4,6,8)
+print()
+
+
+# Now we will write a function tree_to_tuple which will convert a binary tree into a tuple representing the same tree so this function is the reverse of what we did above.
+
+def tree_to_tuple(node):
+    pass
+
+
+
+# We will also write a helper function to display all the keys in a tree like structure for easier visualization 
+
+def display_keys(node, space = '\t', level = 0): # node = current node, space = '\t to put a tab space, level = 0 :- depth of current node (starts from 0 at root)
+
+    # if node is empty
+
+    if node is None: # we do this to represent None in the tree when there is no child in the connecting node we depict it by '∅'. To put it simply when recursion hit the missing child we depict it by '∅'
+        print(space*level + '∅')
+        return 
+    
+    # if node is leaf
+
+    if node.left is None and node.right is None: # this code block means is that if the node that we are currently on is a leaf having no left or right child
+        print(space*level + str(node.key)) # print its value in the correct position 
+        return # then stop going deeper and return 
+    
+    # if nodes have children
+
+    display_keys(node.right, space, level + 1)
+    print(space*level + str(node.key))
+    display_keys(node.left,space,level + 1)
+
+display_keys(tree2)
+print('\n')
+print('\n')
+
+tree_tuple2 = ((2),1,(3))
+tree3 = parse_tuple(tree_tuple2)
+display_keys(tree3)
+print('\n')
+print('\n')
+
+# For this the Call-1 happens in display_keys as display_keys(node=1,level=0)
+ 
+# Then it checks the if condition which turns out to be False as node is not None
+
+# Then it checks our 2nd if condition which again turns out to be False because of node do have a left and right child so this condition also gets skipped
+ 
+# so the function moves to display_keys(node.right,space,level+1) and here the current value of node.right = 3, space = '\t', level = 0 so our line becomes display_keys(3,'\t',0+1=1)
+
+# At this point Call 1 is paused at display_keys(node.right,space,level+1) and will only move further in its own block after this is solved.
+
+# Now Call-2 happens display_keys(node = 3, level = 1)
+
+# so we check our 1st if condition again which turns out to be False
+ 
+# Then we check our 2nd if condition which turns out to be True because node 3 dont have a left or right child
+ 
+# so now we execute the next line print(space*level + str(node.key)) which becomes print('\t' * 1 + '3') = print('\t3') so output becomes     3 . and then it gets returned upwards because this Call 2 is finished now.
+ 
+# now we come back to Call 1, we have solved and printed right subtree which is (3)
+
+# now we move to this line in Call 1 which is print(space*level + str(node.key)) and here the values are space='\t' * level=0 + node.key = 1 so that makes it str(1) and we print 1 
+
+# Then we move to next line in Call 1 which is display_keys(node.left, space, level+1) which when substituted the values in becomes node.left = 2, space = '\t', level = 0 and that looks display_keys(2, '\t', 1)
+
+# Now we enter Call 3 
+
+# display_keys(node=2,level=1)
+
+# we check our 1st if condition which turns out as False then we move to 2nd if condition which turns out to be true 
+
+# so we do print(space*level + str(node.key)) which becomes print('\t' * 1 + '2') and prints     2 . and returns the node 
+
+# and we are done with the function 
+
+
+# Traversing the binary tree 
+
+# Traversal in a binary tree means moving through the tree in a specific order and visiting every node exactly one time.
+
+# Visiting a node simply means performing some action on it such as printing its value, storing it in a list, counting it, or using it in some logic.
+
+# Different traversal orders visit the same nodes in different sequences, which is why traversal is important.
+
+
+# Depth First Traversal (DFS) is a way of traversing a tree in which we go as deep as possible along one branch before backtracking and exploring other branches.
+# It explores nodes level by level in depth rather than visiting all nodes at the same level.
+# Inorder, Preorder, and Postorder traversals are all types of depth first traversal.
+
+# INORDER TRAVERSAL 
+
+# Inorder traversal is a depth first traversal technique in which we first visit the left subtree, then the current node, and finally the right subtree.
+# In a Binary Search Tree (BST), inorder traversal visits the nodes in sorted order.
+# Rule :- Left -> Node -> Right
+
+# PREORDER TRAVERSAL
+
+# Preorder traversal is a depth first traversal technique in which we first visit the current node, then the left subtree, and finally the right subtree.
+# This traversal is useful when we want to process the root before its children.
+# Rule :- Node -> Left -> Right
+
+# POSTORDER TRAVERSAL 
+
+# Postorder traversal is a depth first traversal technique in which we first visit the left subtree, then the right subtree, and finally the current node.
+# This traversal is useful when we want to process child nodes before their parent.
+# Rule :- Left -> Right -> Node
+
+
+# now we will write code for each 
+
+def traversal_inorder(node): # This function takes one node and returns the inorder traversal of the tree rooted at that node
+    if node is None: # if no node return an empty list
+        return []
+    return (traversal_inorder(node.left) + [node.key] + traversal_inorder(node.right)) # to put it simply this means inorder traversal of this tree is inorder traversal of left subtree then current node then inorder traversal of right subtree. Give me inorder traversal list of left subtree + the current node + inorder traversal list of right subtree
+
+print(traversal_inorder(tree3))
+print()
+
+# when we call this function the node = 1 and since the node is not None we execute and return the 2nd return condition which becomes traversal_inorder(2) + [1] + traversal_inorder(3) # this is call 1
+
+# But the code cant return it yet because the traversal_inorder(2) and traversal_inorder(3) are not yet solved 
+
+# so call 2 happens with node = 2 and we again execute the 2nd return statement which comes out as traversal_inorder(None) + [2] + traversal_inorder(None)
+
+# now a call which i will designate as call 2.1 happens inside call 2 to solve traversal_inorder(None) which fulfills the 1st condition where node is None and return us with an empty list [] it returns the empty list back to call 2 and it makes our line return [] + [2] 
+
+# Then call 2.2 happens inside call 2 to solve the right side traversal_inorder(None) which returns [] upwards to call 2 and now we have return [] + [2] + [] which becomes [2] now call 2 is finished and return this upward to call 1 where traversal_inorder(2) becomes [2] and our line becomes [2] + [1] + traversal_inorder(3)
+
+# now we solve for traversal_inorder(3) so here our call 3 starts and same call 3.1 and 3.2 happens inside it which returns empty list because there are no nodes left or right of node 3 and call 3 returns [] + [3] + [] = [3] upwards to call 1
+
+# now call 1 return statement looks like [2] + [1] + [3] = [2,1,3] which gets returned to us 
+
+
+
+def traversal_preorder(node):
+    if node is None:
+        return []
+    return ([node.key] + traversal_preorder(node.left) + traversal_preorder(node.right))
+
+print(traversal_preorder(tree3))
+print()
+
+# call 1 we call the function and our 2nd return statement becomes return ([1] + traversal_preorder(2) + traversal_preorder(3))
+
+# now call 2 happens and we call traversal_preorder(2), it returns ([2] + [] + []) = [2] with call 2.1 and 2.2 also happening which get returned upwards to call 1, which becomes return [1] + [2] + traversal_preorder(3)
+
+# now call 3 happens, we call traversal_preorder(3) which executes 2nd return statement and internally also execute call 3.1 and 3.2 ultimately we get [3] + [] + [] = [3] and it gets returned to call 1 which becomes [1] + [2] + [3] = [1,2,3] 
+
+
+def traversal_postorder(node):
+    if node is None:
+        return []
+    return (traversal_postorder(node.left) + traversal_postorder(node.right) + [node.key])
+
+print(traversal_postorder(tree3))
+print()
+
+# call 1 happens by calling traversal_postorder(tree3) and it executes 2nd return statement which becomes return (traversal_postorder(2) + traversal_postorder(3) + [1])
+
+# now call 2 happens to solve traversal_postorder(2) with internal calls 2.1 and 2.2, this call returns [] + [] + [2] = [2] to call 1 upwards and call 1 now becomes return [2] + traversal_postorder(3) + [1]
+
+# now call 3 happens to solve traversal_postorder(3) with internal calls 3.1 and 3.2, this call returns [] + [] + [3] = [3] upwards to call 1 and now our line in call 1 becomes return [2] + [3] + [1] = [2,3,1] as the final answer
+
+
+# now we will add code for getting tree height/depth and tree size (count number of nodes) too
+
+
+def tree_height(node): # This function takes the node and returns the height of the tree rooted at that node
+    if node is None: # base case if there is no node 
+        return 0
+    return 1 + max(tree_height(node.left), tree_height(node.right)) # recursive case that means the height is 1 for this node + the bigger height between left or right subtree that why we are using the max function which is used to find the largest item in an iterable data structure or largest of the 2 arguments 
+
+print(tree_height(tree3))
+print(tree_height(tree2))
+print()
+
+# for tree3 call 1 happens with node = 1 so the 2nd return condition gets executed which makes it 1 + max(tree_height(2), tree_height(3)) and this pauses here 
+
+# now call 2 happens with calling tree_height(2) and inside it executes the 2nd return condition which makes it 1 + max(tree_height(None), tree_height(None)), now it again pauses here and call 2.1 happens for tree_height(None) which returns 0 as here the node is None this gets returned to call 2 which becomes 1 + max(1,tree_height(None)) then call 2.2 happens which also returns 0 and now our total return value of whole call 2 becomes 1 + 0 + 0 = 1 so 1 gets returned back to call 1 and it makes our line in call 1 = 1 + max(1,tree_height(3))
+
+# now call 3 happens for tree_height(3) and inside it execution of 2nd return statement happens which makes the line = 1 + max(tree_height(None), tree_height(None)) then internal 3.1 and 3.2 calls happen and they return 0 , 0 and that makes our whole call 3 value be 1 + 0 + 0 = 1 and 1 gets returned back to call 1 which makes it 1 + max(1,1) and since both inside max are 1 so we take 1 which makes our expression 1 + 1 = 2 as our answer
+
+
+def tree_size(node): # This function takes a node and returns the total number of nodes in the tree rooted at that node
+    if node is None: # base case
+        return 0
+    return 1 + tree_size(node.left) + tree_size(node.right) # recursive case that means size is 1 for current node + size of left subtree + size of right subtree
+
+print(tree_size(tree3))
+print(tree_size(tree2))
+print()
+
+
+# for tree3 call 1 happens and 2nd return statement gets executed that makes our line = return 1 + tree_size(2) + tree_size(3) now we pause here
+
+# call 2 happens for tree_size(2) and here also 2nd return statement gets executed that makes the line = return 1 + tree_size(None) + tree_size(None) and then this pauses and call 2.1 and 2.2 happens which return 0,0 as the node is None and that makes our return expression of call 2 as 1 + 0 + 0 = 1 and this gets returned back to call 1 which makes call 1 line = return 1 + 1 + tree_size(3)
+
+# now call 3 happens for tree_size(3) again its 2nd return statement gets executed which makes it return 1 + tree_size(None) + tree_size(None) which then pauses here and call 3.1 and 3.2 happens and returns 0,0 that makes our call 3 return expression as 1 + 0 + 0 = 1 which gets returned to call 1 and that makes its return expression as 1 + 1 + 1 = 3 as the final answer 
